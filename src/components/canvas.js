da2b75720153f8e95282e3bed495235bbf2cbcba
Musyoka Philip <87429036+Musyonchez@users.noreply.github.com>
2023-12-08 11:25:07 +0300

    update

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -3,48 +3,62 @@ import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 
 const CropImage = () => {
-  const [src, setSrc] = useState(null);
-  const [image, setImage] = useState();
+ const [src, setSrc] = useState(null);
+ const [image, setImage] = useState();
 
-  const onFileChange = (e) => {
+ const onFileChange = (e) => {
     e.preventDefault();
     let files = e.target.files;
     let file = files[0];
     if (file) {
-      setSrc(URL.createObjectURL(file));
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        if (img.width >= 600 && img.height >= 400) {
+          setSrc(URL.createObjectURL(file));
+        } else {
+          alert('Please upload an image with a minimum width and height of 400 pixels.');
+        }
+      };
     }
-  };
+ };
 
-  const onCrop = () => {
+ const onCrop = () => {
     if (image && image.cropper) {
       setSrc(image.cropper.getCroppedCanvas().toDataURL());
     }
-  };
+ };
 
-  const downloadImage = () => {
+ const downloadImage = () => {
     const link = document.createElement('a');
     link.href = image.cropper.getCroppedCanvas().toDataURL();
     link.download = 'cropped-image';
     link.click();
-  };
+ };
 
-  return (
+ return (
     <div>
       <input type="file" onChange={onFileChange} />
       {src && (
         <Cropper
           src={src}
-          style={{ height: 400, width: '100%' }}
+          style={{ minHeightheight: 600, minWidth: 400, objectFit: 'cover' }}
           ref={(node) => {
             setImage(node);
+          }}
+          cropperOptions={{
+            aspectRatio: 16 / 9,
+            viewMode: 1,
+            minCropBoxWidth: 400,
+            minCropBoxHeight: 400,
           }}
         />
       )}
       <button onClick={onCrop}>Crop</button>
-      <br/>
+      <br />
       <button onClick={downloadImage}>Download</button>
     </div>
-  );
+ );
 };
 
 export default CropImage;
