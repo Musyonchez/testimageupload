a4c35bb2a21d0ed21b0064b885eb6d6336fbb0f8
Musyoka Philip <87429036+Musyonchez@users.noreply.github.com>
2023-12-08 08:17:13 +0300

    upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,13 @@
-// pages/index.js
 import React from 'react';
+import CropImage from '../components/canvas';
 
-const Home = () => {
-  const handleImageUpload = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append('image', e.target.elements.image.files[0]);
-
-    try {
-      const response = await fetch('http://localhost:5000/upload', {
-        method: 'POST',
-        body: formData,
-      });
-
-      const data = await response.json();
-      console.log('Image uploaded successfully:', data.message);
-    } catch (error) {
-      console.error('Error uploading image:', error.message);
-    }
-  };
-
-  return (
+const Crop = () => {
+ return (
     <div>
-      <form onSubmit={handleImageUpload}>
-        <input type="file" name="image" accept="image/*" />
-        <button type="submit">Upload Image</button>
-      </form>
+      <h1>Crop Image</h1>
+      <CropImage />
     </div>
-  );
+ );
 };
 
-export default Home;
+export default Crop;
